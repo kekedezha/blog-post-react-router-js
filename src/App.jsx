91cbac7aecb76c3a6d5b874e0dd7689cc424d880
kekedezha <dezhac6@gmail.com>
2024-06-25 16:04:45 -0400

    updated project with routes to be used. created components to be used

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
 
 function App() {
-
   return (
-    <>
- 
-    </>
-  )
+    <div>
+      <Header />
+      <Nav />
+      <Routes>
+        {/* Base URL shows the Home component */}
+        <Route exact path="" element={<Home />} />
+
+        {/* /post URL shows the NewPost component */}
+        <Route exact path="post" element={<NewPost />} />
+
+        {/* /post/:id URL shows details of a specific post through the PostPage component */}
+        <Route path="post/:id" element={<PostPage />} />
+
+        {/* /about URL shows the About component */}
+        <Route path="about" element={<About />} />
+
+        {/* Wild card to catch any other path that may be entered as a url for this website. Redirected to Missing component. */}
+        <Route path="*" element={<Missing />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
